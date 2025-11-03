9ff5868bb9ac5d96f92cfd4e9eb20dec4592f478
Shankalpa Duwadi <duwadishankalpa@gmail.com>
2025-11-03 14:03:19 -0500

    added users route

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   console.log("here");
-  res.render("index");
+  res.render("index", { user: "Shawn" });
+});
+app.get("/users", (req, res) => {
+  res.send("Users List");
 });
 
+app.get("/users/new", (req, res) => {
+  res.send("New User Form");
+});
 app.listen(3030);
