fdf8f770f1eb6eb93c31c1f084207f6cfcd2e061
Shankalpa-D <DuwadiShankalpa@gmail.com>
2025-11-11 22:10:43 -0500

    set up posts route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
 const app = express();
 app.set("view engine", "ejs");
 app.use("/users", userRouter);
+app.use("/posts", postRoutes);
 app.get("/", (req, res) => {
   console.log("here");
   res.render("index", { user: "Shawn" });
