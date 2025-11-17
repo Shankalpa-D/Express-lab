358b4562b56abdbbedb4f064320cb4e5ad9d277b
Shankalpa-D <DuwadiShankalpa@gmail.com>
2025-11-17 14:14:23 -0500

    made new ejs file and forms

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Users List");
+  res.render('users/list', { users:users });
 });
 
 router.get("/new", (req, res) => {
   res.send("New User Form");
+});
+
+router.post("/", (req, res) => {
+  res.send(" User Created");
+  const firstName = req.body.firstName;
+  const isValid = firstName !== "";
+  if(isValid) {
+    
+  console.log('Adding User: ${firstName}');
+  users.push({ name:firstName});
+  console.log('New Set od users: ${users}');
+
+  } else {
+    console.log('Error adding user!');
+    res.render('users/new', { firstName:firstName });
+  }
 });
 
 router
@@ -20,4 +36,9 @@ router
   .put((req, res) => {
     res.send(`Updating User ID: ${req.params.id}`);
   });
+
+  const users = [
+    { name: "Shawn" },
+    { name: "John", },
+  ];
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
+
+
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+
+
 app.get("/", (req, res) => {
   console.log("here");
   res.render("index", { user: "Shawn" });
 });
 app.listen(3030);
+app.get('/new', (req, res) => {
+  res.render('users/new', {firstName: "Please enter ur name"});
+});
+
+function logger(req, res, next) {
+  console.log(`page accessed: ${req.originalUrl}`);
+  next();
+}
