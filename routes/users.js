355450bb1ea25b26de95942920da4d1affdc6e5e
Shankalpa-D <DuwadiShankalpa@gmail.com>
2025-11-19 14:10:37 -0500

    added more user data

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,37 @@ router.get("/", (req, res) => {
   res.render('users/list', { users:users });
 });
 
+
+router.get('/new', (req, res) => {
+  res.render('users/new', {firstName: ""});
+});
+
+
 router.get("/new", (req, res) => {
   res.send("New User Form");
 });
 
 router.post("/", (req, res) => {
-  res.send(" User Created");
+ 
   const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const age = req.body.age;
+  const gender = req.body.gender;
   const isValid = firstName !== "";
   if(isValid) {
     
   console.log('Adding User: ${firstName}');
-  users.push({ name:firstName});
+  users.push({ firstName:firstName, lastName:lastName, gender:gender, age:age});
   console.log('New Set od users: ${users}');
+  res.send(" User Created");
 
   } else {
     console.log('Error adding user!');
     res.render('users/new', { firstName:firstName });
   }
 });
+
+
 
 router
   .route("/:id")
--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const postRoutes = require("./routes/posts");
 
 const app = express();
 app.set("view engine", "ejs");
-<<<<<<< Updated upstream
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 
-=======
->>>>>>> Stashed changes
+
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
@@ -22,9 +21,7 @@ app.get("/", (req, res) => {
   res.render("index", { user: "Shawn" });
 });
 app.listen(3030);
-app.get('/new', (req, res) => {
-  res.render('users/new', {firstName: "Please enter ur name"});
-});
+
 
 function logger(req, res, next) {
   console.log(`page accessed: ${req.originalUrl}`);
